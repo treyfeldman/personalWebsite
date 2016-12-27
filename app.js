9e0c23592c17db5881569485c311768f084f6ae2
treyfeldman <tf912@nyu.edu>
2016-12-27 02:51:56 +0000

    Fixed navbar for all pages

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use('/pdfs', express.static(__dirname + '/pa'));
 
 app.use('/', routes);
 
--- public/javascripts/mainScript.js
+++ public/javascripts/mainScript.js
@@ -1,17 +1,23 @@
 $(window).ready(function() {
-	$("link-bar").scroll(function() {
-		console.log("hey");
-		if($(this).scrollTop() >= 150) {
-		    $('link-bar').addClass('stick');
+	console.log("page loaded");
+	$(window).scroll(function() {
+		if($(this).scrollTop() >= 135) {
+		    $('.link-bar').addClass('stick');
+		    $('#resume').addClass('stick-content');
+		    $('#photos').addClass('stick-content');
+		    if($(this).scrollTop() < 170) {
+			$('.mini-trey-logo').css({'left':$(this).scrollTop()-135});
+		    }
+		    
+		    $('.mini-trey-logo').css({'opacity':(($(this).scrollTop()-135)/25)});
 		}
 		else {
-		    $('link-bar').removeClass('stick');
+		    $('.link-bar').removeClass('stick');
+		    $('#resume').removeClass('stick-content');
+		    $('#photos').removeClass('stick-content');
+		    $('.mini-trey-logo').css('opacity', 0);
 		}
 	    });
 
-	$("p").click(function() {
-		$("p").hide();
-		console.log("hi");
-	    });
 	
     });
